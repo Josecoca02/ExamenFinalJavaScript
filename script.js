faf7cbaecc1422352e828ac61f3c4c99ef3434e1
Josecoca02 <115150610+Josecoca02@users.noreply.github.com>
2022-12-16 12:25:23 +0100

    primos avances del examen

---
+++ script.js
@@ -0,0 +1,31 @@
+const body = document.querySelector('body')
+const myTimeout = setTimeout(mostrarSaludo, 3000);
+
+function mostrarSaludo() {
+document.getElementById("texto").style.visibility="visible";
+}
+
+
+const crearNeive = () => {
+    let copo = document.createElement('i')
+    let x = innerWidth * Math.random()
+    let size = (Math.random() * 8) + 2
+    let z = Math.round(Math.random()) * 100
+    let delay = Math.random() * 5
+    let anima = (Math.random() * 10) + 5
+
+    copo.style.left = x + 'px'
+    copo.style.width = size + 'px'
+    copo.style.height = size + 'px'
+    copo.style.zIndex = z
+    copo.style.animationDelay = delay + 's'
+    copo.style.animationDuration = anima + 's'
+
+    body.appendChild(copo)
+
+    setTimeout(() => {
+        copo.remove()
+    }, anima * 1000)
+}
+
+setInterval(crearNeive, 50);
